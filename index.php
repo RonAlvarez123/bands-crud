c250edd3a12b6ddfec77e403fe96c6088e6af873
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-07-05 23:03:03 +0800

    added links that jquery will use

--- index.php
+++ index.php
@@ -30,50 +30,54 @@ for ($i = 0; $i < count($bandsXml); $i++) {
     <link rel="stylesheet" href="css/globalStyles.css">
     <link rel="stylesheet" href="css/index.css">
     <title>OPM Bands</title>
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
 </head>
 
 <body>
 
-    <div class="container">
-        <header>
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="" class="search">
-                <input type="text" placeholder="Search band...">
-                <button type="submit" class="btn-click">
-                    <img src="svg/search-solid.svg" alt="">
-                </button>
-            </form>
-            <section class="links">
-                <a href="" class="a-click btn-update">Update</a>
-                <a href="" class="a-click btn-delete">Delete</a>
-            </section>
-        </header>
+    <div class="main-container">
+        <div class="container">
+            <header>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="" class="search">
+                    <input type="text" placeholder="Search band...">
+                    <button type="submit" class="btn-click">
+                        <img src="svg/search-solid.svg" alt="">
+                    </button>
+                </form>
+                <section class="links">
+                    <a href="updateBand.php" class="a-click btn-update">Update</a>
+                    <a href="deleteBand.php" class="a-click btn-delete">Delete</a>
+                </section>
+            </header>
 
-        <main>
-            <section class="header">
-                <p>#</p>
-                <p>BandName</p>
-                <p>Debut</p>
-                <p>HitSong</p>
-                <p>Genres</p>
-            </section>
-            <div class="bands">
-                <?php foreach ($bands as $band) { ?>
-                    <section>
-                        <p><?php echo $band['bandCode'] ?></p>
-                        <p><?php echo $band['bandName'] ?></p>
-                        <p><?php echo $band['debut'] ?></p>
-                        <p><?php echo $band['hitSong'] ?></p>
-                        <p><?php echo $band['genres'] ?></p>
-                    </section>
-                <?php } ?>
-            </div>
-        </main>
+            <main>
+                <section class="header">
+                    <p>#</p>
+                    <p>BandName</p>
+                    <p>Debut</p>
+                    <p>HitSong</p>
+                    <p>Genres</p>
+                </section>
+                <div class="bands">
+                    <?php foreach ($bands as $band) { ?>
+                        <section>
+                            <p><?php echo $band['bandCode'] ?></p>
+                            <p><?php echo $band['bandName'] ?></p>
+                            <p><?php echo $band['debut'] ?></p>
+                            <p><?php echo $band['hitSong'] ?></p>
+                            <p><?php echo $band['genres'] ?></p>
+                        </section>
+                    <?php } ?>
+                </div>
+            </main>
 
-        <footer>
-            <a href="addBand.php">Add Band</a>
-        </footer>
+            <footer>
+                <a href="addBand.php" class="a-click">Add Band</a>
+            </footer>
+        </div>
     </div>
 
+    <script src="js/a_click_jquery.js"></script>
 </body>
 
 </html>
