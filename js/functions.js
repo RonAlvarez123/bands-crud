d1e6a1893ae836cdd91ec4ef73335c7af5a0cf59
RonAlvarez123 <homfatigueqwe123@yahoo.com>
2021-07-06 23:18:09 +0800

    added functions js file

---
+++ js/functions.js
@@ -0,0 +1,46 @@
+function fetchAllBands() {
+    let bandValues = {
+        allBands: [],
+        allBandCodes: [],
+        allBandNames: []
+    };
+
+    xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (this.status == 200) {
+            let response = xhr.responseXML
+            let bands = Array.from(response.getElementsByTagName('band'));
+            bands.forEach(band => {
+                let genres = [];
+                Array.from(band.children[3].children).forEach(genre => {
+                    genres.push(genre.innerHTML);
+                });
+
+                bandValues.allBands.push({
+                    code: band.getAttribute('bandCode'),
+                    name: band.children[0].innerHTML,
+                    debut: band.children[1].innerHTML,
+                    hitSong: band.children[2].innerHTML,
+                    genres: genres
+                });
+
+                bandValues.allBandCodes.push(band.getAttribute('bandCode'));
+                bandValues.allBandNames.push(band.children[0].innerHTML);
+            });
+        }
+    }
+    xhr.open('GET', 'IconicOpmBands.xml', false);
+    xhr.send();
+
+    return bandValues;
+}
+
+function findBandMatches(word, bandData) {
+    let result = [];
+    bandData.allBandNames.forEach((item, index) => {
+        if (item.includes(word)) {
+            result.push([item, bandData.allBands[index].debut]);
+        }
+    });
+    return result;
+}
